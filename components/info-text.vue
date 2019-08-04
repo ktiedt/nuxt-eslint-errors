721c198aa6890bd4c01d7f3dff5fea96871c4b81
Karl Tiedt <ktiedt@gmail.com>
2019-08-04 12:57:29 -0500

    Commit to learn why eslint breaks things

---
+++ components/info-text.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-tooltip right>
+    <template v-slot:activator="{ on }">
+      <span class="info-text" v-on="on"><slot /></span>
+    </template>
+    <span>{{ tooltip }}</span>
+  </v-tooltip>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+
+@Component
+class InfoText extends Vue {
+  @Prop({ default: '' }) tooltip!: string;
+}
+
+export default InfoText
+</script>
+
+<style lang="scss" scoped>
+.info-text {
+  border-bottom: 1px dotted aqua;
+  display: inline-block;
+}
+
+p {
+  text-align: left;
+}
+</style>
